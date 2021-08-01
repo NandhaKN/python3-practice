771ced5ce11365389a93b9f3133ce24d5f265641
snandha99 <snandha99@gmail.com>
2021-08-01 12:52:19 +0530

    some changes odne it

--- Oddoneout.py
+++ Oddoneout.py
@@ -1,5 +1,5 @@
-fruit_master = ['aathi','nagapayam','mango','grapes','guavo']
-checkfruits = ['aathi','nagapayam','mt15',"herohonda",'grapes']
+fruit_master = ['Orange','apple','mango','grapes','blackberry','cherry']
+checkfruits = ['Orange','apple','github',"visualstudio",'grapes']
 for fruit in checkfruits:
     if not fruit in fruit_master:
         print (fruit)
